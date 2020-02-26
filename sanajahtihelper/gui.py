e547f4ce86c2fb96f576d56cc1557e33f1b73d7f
Jaleksi <aleksi.jok@gmail.com>
2020-02-26 21:42:21 +0200

    add(gui): create widget classes

--- sanajahtihelper/gui.py
+++ sanajahtihelper/gui.py
@@ -1,29 +1,44 @@
 from PyQt5 import QtWidgets
 
 
-class WordGrid(QtWidgets.QWidget):
-    def __init__(self, parent):
-        super().__init__()
-        self.parent = parent
-        layout = QtWidgets.QVBoxLayout(self)
-        teksti = QtWidgets.QLabel(self)
-        teksti.setText('asd')
-        layout.addWidget(teksti)
-
-class WordList(QtWidgets.QWidget):
-    def __init__(self, parent):
-        super().__init__()
-        self.parent = parent
-
 class MainWindow(QtWidgets.QMainWindow):
-    def __init__(self):
+    def __init__(self, parent=None):
         super().__init__()
-
-        self.main_widget = QtWidgets.QWidget()
-        layout = QtWidgets.QHBoxLayout(self.main_widget)
-
         self.word_grid = WordGrid(self)
         self.word_list = WordList(self)
 
-        layout.addWidget(self.word_grid)
-        layout.addWidget(self.word_list)
+    def master_layout(self):
+        self.master_layou
+        self.master_layout.addWidget(self.word_list)
+        self.master_layout.addWidget(self.word_grid)
+        self.setCentralWidget(self.word_grid)
+
+
+class WordGrid(QtWidgets.QWidget):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.controls()
+        self.layout()
+
+    def controls(self):
+        self.button = QtWidgets.QPushButton("Button")
+
+    def layout(self):
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.button)
+        self.setLayout(self.layout)
+
+
+class WordList(QtWidgets.QWidget):
+    def __init__(self, parent):
+        super().__init__(parent)
+        self.controls()
+        self.layout()
+
+    def controls(self):
+        self.label = QtWidgets.QLabel("BuAtatAon")
+
+    def layout(self):
+        self.layout = QtWidgets.QVBoxLayout(self)
+        self.layout.addWidget(self.label)
+        self.setLayout(self.layout)
